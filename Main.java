e3fb9bbc6615bdcea47026bba51c5d7a8b68772c
Saralla-Rohit <rohitsarla69@gmail.com>
2026-05-18 12:38:45 +1200

    static onstatic access through obj OMG!!!

--- Main.java
+++ Main.java
@@ -1,37 +1,17 @@
 package com.rohit;
 import java.util.*;
-class Main {
-   
-    public static void main(String[] args) {
-       /*
-        String s1="rohit";
-        String s2="rohit";
-        IO.print(s1==s2);//true
-        */
 
-        /*
-        String s1="rohit";
-        String s2=s1;
-        IO.print(s1==s2);//true
-        */
-        
-        /*
-        String s1="rohit";
-        String s2=new String("rohit");
-        IO.print(s1==s2);//false
-        */
-        
-        
-        /*
-        String s1=new String("rohit");
-        String s2=s1;
-        IO.print(s1==s2);//true
-        */
-        
-        /*
-        String s1=new String("Rohit");
-        String s2=new String("Rohit");
-        IO.print(s1==s2);//false
-        */
+class Main {
+    void rem(){
+        IO.println("rem");
+    }
+    static void rem1(){
+        IO.println("rem1");
+    }
+    public static void main(String[] args) {
+        Main m=new Main();
+        m.rem();
+        m.rem1();//still allowed
+        Main.rem1();//recommended
     }
 }
