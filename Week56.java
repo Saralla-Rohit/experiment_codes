5388f7a5c5c42e4b4870a7e98e82e96328983be3
Saralla-Rohit <rohitsarla69@gmail.com>
2026-06-03 14:57:57 +1200

    isomorphic string

--- Week56.java
+++ Week56.java
@@ -1,28 +1,54 @@
-class Week56{
-    
-    public void main(String[] args) {
-        String str="The number returned should not";
-        String strs[]=str.split(" ");
-        String res="";
-        for(String st:strs){
-            String rev=rev(st);
-            res+=rev;
-            res+=' ';
-        }
-        IO.print(res);
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
-    }
-    public String rev(String str){
-        char []ch=str.toCharArray();
-        int i=0;
-        int j=str.length()-1;
-        while(i<=j){
-            char temp=ch[i];
-            ch[i]=ch[j];
-            ch[j]=temp;
-            i++;
-            j--;
+class Week56{
+    public boolean isIso(String s1,String s2){
+        int n=s1.length();
+        if(s1.length()!=s2.length()){
+            return false;
         }
-        return new String(ch);
+        HashMap<Character,Character>map=new HashMap<>();
+        Set<Character>used=new HashSet<>();
+        for(int i=0;i<n;i++){
+            char ch1=s1.charAt(i);
+            char ch2=s2.charAt(i);
+            // If ch1 already has a mapping
+            if(map.containsKey(ch1)){
+                // Mapping must remain consistent
+                // Example:
+                // p -> t initially
+                // later p -> l is invalid
+                if(map.get(ch1)!=ch2){
+                    return false;
+                }
+            }else{
+                // ch1 is appearing for the first time
+                // If ch2 is already mapped by another character,
+                // then two characters are mapping to the same target
+                // Example:
+                // a -> x
+                // b -> x  (invalid)
+                if(used.contains(ch2)) {
+                    return false;
+                }
+
+                // Store the new mapping
+                map.put(ch1,ch2);
+
+                // Mark target character as used
+                used.add(ch2);
+            }
+        }
+        return true;
+    }
+    public void main(String[] args) {
+        String s1="paper";
+        String s2="title";
+        boolean res=isIso(s1, s2);
+        if(res){
+            IO.print("valid");
+        }else IO.print("not valid");
+        
     }
 }
